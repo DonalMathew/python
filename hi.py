c8a53bfdc53a42432457e251dab4f0525ef05746
DonalMathew <92646475+DonalMathew@users.noreply.github.com>
2023-10-12 22:05:17 +0530

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,11 @@
+from datetime import date
+print('Hello, World!')
+print(date.today())
+print("Today's date is: " + str(date.today()))
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
+
+print("hello {str(first_number)} htere")
+print(first_number+"hello")
